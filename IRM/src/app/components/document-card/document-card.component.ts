009712443a4527be712197ed570e7bdd38d7b56c
Weqil <ar5ipooska@yandex.ru>
2024-09-25 23:00:20 +0500

    add documents

--- IRM/src/app/components/document-card/document-card.component.ts
+++ IRM/src/app/components/document-card/document-card.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Theme } from '../../models/theme';
 import { CommonModule } from '@angular/common';
 import { IPdf } from '../../models/pdf';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-document-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './document-card.component.html',
   styleUrl: './document-card.component.scss'
 })
